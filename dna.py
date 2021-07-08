9866fb9d3395d8ecddacb567e279d6b5c4dbdb75
Ksuuuuuu <87008317+Ksuuuuuu@users.noreply.github.com>
2021-07-08 16:45:30 +0300

    Update dna.py

--- dna.py
+++ dna.py
@@ -1,7 +1,33 @@
 import csv
-reader=csv.reader(open('databases/small.csv'))
-result={}
+name_csv=input("Введите имя файла с данными\n")
+reader= csv.reader(open(name_csv))
+D={}
 for row in reader:
     key=row[1:]
-    result[key]=row[0]
-print result
+    D[key]=row[0]
+name_txt=input("Введите имя файла с днк искомого человека")
+with open(name_txt, 'r') as file:
+    str=file.read()
+lst_count=[]
+first_key=list(D.keys())[0]
+for i in first_key:
+    count=0
+    first_pos=str.find(i)
+    if first_pos !=-1:
+        count=1
+        size=len(i)
+        buffer=str[first_pos+size:first_pos+2*size-1]
+        while i==buffer:
+            count++
+            buffer=str[first_pos+size*count:first_pos+size*count+size-1]
+    lst_count.append(count) 
+res=D.get(lst_count)
+if res:
+    print(res)
+else:
+    print('No match')
+    
+
+        
+        
+        
